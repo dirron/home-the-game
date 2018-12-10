c4f488907454eda3ebfdf724c04b8809a4baa87b
Dirron <dillonoudomsouk@gmail.com>
2018-12-10 13:06:01 -0500

    fix camera going outside of bounds

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class MainCamera : MonoBehaviour {
 
+    public GameObject backgroundSprite;
+
     private GameObject player;
     private Vector3 cameraOffset = new Vector3(0, 2, -10);
-    private Vector2 levelExit;
+    private GameObject levelExit;
     private bool levelExitExists;
 
     private float startingPosX;
+
+    private float camW;
+    private float camH;
+    private float endCamBoundary;
+    private float topCamBoundary;
 
     void OnEnable ()
     {
@@ -26,37 +34,61 @@ public class MainCamera : MonoBehaviour {
     // Use this for initialization
     void Start () {
         startingPosX = transform.position.x;
+        camH = Camera.main.orthographicSize * 2f;
+        camW = camH * Camera.main.aspect;
+        topCamBoundary = backgroundSprite.transform.position.y
+            + backgroundSprite.GetComponent<SpriteRenderer>().bounds.size.y / 2;
     }
 
     // Update is called once per frame
     void Update () {
-        if (levelExitExists && player != null)
-        {
-            float horizontalDistanceToExit = levelExit.x - player.transform.position.x;
-
-            if (player.transform.position.x >= startingPosX
-                && horizontalDistanceToExit > 3 && player.transform.position.x > 1)
-            {
-                transform.position = player.transform.position + cameraOffset;
-            }
-            else
-            {
-                float x = transform.position.x;
-                float y = player.transform.position.y + cameraOffset.y;
-
-                transform.position = new Vector3(x, y, transform.position.z);
-            }
-        }
+        Move();
     }
 
     void OnPlayerSpawned()
     {
-        this.player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void OnLevelExitCreated()
     {
-        levelExit = GameObject.FindGameObjectWithTag("LevelExit").transform.position;
+        levelExit = GameObject.FindGameObjectWithTag("LevelExit");
+
+        endCamBoundary = levelExit.transform.position.x + levelExit.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+
         levelExitExists = true;
+    }
+
+    void Move()
+    {
+        if (levelExitExists && player != null)
+        {
+            float distToEndX = endCamBoundary - player.transform.position.x;
+            float distToEndY = topCamBoundary - (player.transform.position.y + cameraOffset.y);
+            float x;
+            float y;
+
+            if (player.transform.position.x > startingPosX && distToEndX > camW / 2)
+            {
+                x = player.transform.position.x;
+            }
+            else
+            {
+                x = transform.position.x;
+            }
+
+            if (distToEndY > camH / 2)
+            {
+                Debug.Log("First");
+                y = player.transform.position.y + cameraOffset.y;
+            }
+            else
+            {
+                Debug.Log("Second");
+                y = Mathf.Min(transform.position.y, topCamBoundary - camH / 2);
+            }
+
+            transform.position = new Vector3(x, y, cameraOffset.z);
+        }
     }
 }
